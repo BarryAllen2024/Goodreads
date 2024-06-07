5ada45eb7841e442cfc6406d3cd02fbe6b35999a
BarryAllen2024 <103613503+BarryAllen2024@users.noreply.github.com>
2024-06-07 14:17:01 +0000

    Added ability to ask for more books

--- main.py
+++ main.py
@@ -17,13 +17,23 @@ toReadList = []
 for i in range(len(allShelvesList)):
   if allShelvesList[i] == "to-read":
     toReadList.append(allTitlesList[i])
-#goes through the list of titles and adds them to the toReadList if they are in the "to read"
+#goes through the list of titles and adds them to the toReadList if they are in the tbr
 
 for i in range(len(allShelvesList)):
   if allShelvesList[i] == "to-read":
     tbrCount = tbrCount + 1
-#goes through the list of titles and adds 1 to tbrCount if they are in the "to read" (code not used currently)
+#goes through the list of titles and adds 1 to tbrCount if they are in the "to read"
 
 print(random.choice(toReadList))
+#prints a random title from the toReadList
+
+print("\n")
+
+answer = input("How many more do you want? Please type a number: \n")
+for i in range(int(answer)):
+  print("\n")
+  print(random.choice(toReadList))
+  print("\n")
+#prints however many more random books you want
 
 #print(tbrCount) / optional if you want to count number of TBR books
